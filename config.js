1647d8c4d2b6efa6b75a7f50acd7e70fb962678f
Zack Low <zalrcc@rit.edu>
2015-11-02 01:37:32 -0500

    DRYed up server code and added better FE code. Server code has memory leaks due to new sockets being opend in routes, keep an array or something to fix

---
+++ config.js
@@ -0,0 +1,93 @@
+module.exports = {
+	"ari" : {
+		"logo" : "card.png",
+		"sound" : "chirp.mp3"
+	},
+	"atl" : {
+
+	},
+	"bal" : {
+
+	},
+	"buf" : {
+
+	},
+	"car" : {
+		"logo" : "panthers.png",
+		"sound" : "panther.mp3"
+	},
+	"cin" : {
+
+	},
+	"chi" : {
+
+	},
+	"cle" : {
+
+	},
+	"dal" : {
+
+	},
+	"den" : {
+
+	},
+	"det" : {
+
+	},
+	"gb" : {
+
+	},
+	"hou" : {
+
+	},
+	"ind" : {
+		"logo": "colts.png",
+		"sound" : "neigh.mp3"
+	},
+	"jax" : {
+
+	},
+	"kc" : {
+
+	},
+	"mia" : {
+
+	},
+	"min" : {
+
+	},
+	"no" : {
+
+	},
+	"ne" : {
+
+	},
+	"nyg" : {
+
+	},
+	"nyj" : {
+
+	},
+	"oak" : {
+
+	},
+	"phi" : {
+
+	},
+	"pit" : {
+
+	},
+	"sd" : {
+
+	},
+	"sf" : {
+
+	},
+	"sea" : {
+
+	},
+	"stl" : {
+
+	}
+
+}
--- index.js
+++ index.js
@@ -1,72 +1,109 @@
+/**
+ * SportsChirp!
+ * 
+ * Emits an event to connected clients when score is updated.
+ *
+**/
 var express = require('express');
 var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
-var request = require('request');
+var request = require('request-promise');
 var _ = require('lodash');
-var currentScore;
+var config = require('./config.js');
+
+var currentScores;
 
 app.use("/media", express.static(__dirname + '/media'));
+app.use("/dist", express.static(__dirname + '/dist'));
+app.use("/dist/css", express.static(__dirname + '/dist/css'));
+app.use("/dist/js", express.static(__dirname + '/dist/js'));
+app.use("/dist/fonts", express.static(__dirname + '/dist/fonts'));
 
-app.get('/', function(req, res) {
-	res.sendFile(__dirname + '/index.html');
-});
+app.set('view engine', 'ejs')
 
-function findString(searchArray){
-	var return_val = false;
-	_.each(searchArray, function(value){
-		if(value != undefined){
-			if(value.indexOf('Bay') != -1 && value.indexOf('Green') != -1){
-				return_val = value;
-			}
+app.get('/:team', function(req, res) {
+	var gameIndex = _.findIndex(currentScores, function(score){
+		if(score.h == req.params.team || score.v == req.params.team){
+			return true;
+		}else {
+			return false;
 		}
 	});
-	return return_val;
-}
+	if(gameIndex == -1){
+		res.send("Game Not Found");
+	}else {
+		var game = currentScores[gameIndex];
+		//res.send(game.h + ": " + game.hs + " - " + game.v + ": "+game.vs);
+		//res.sendFile(__dirname + "/index.html");
+		res.render('index', {
+			'homeName':game.hnn,
+			'homeScore':game.hs,
+			'homeSound':config[game.h.toLowerCase()].sound,
+			'homeLogo': config[game.h.toLowerCase()].logo,
+			'awayName':game.vnn,
+			'awayScore':game.vs,
+			'awaySound':config[game.v.toLowerCase()].sound,
+			'awayLogo': config[game.v.toLowerCase()].logo
+		});
 
-function getScore() {
+		io.on('connection', function(socket) {
+			var lastGameState = currentScores[gameIndex];
+		    
+		    console.log('a user connected to '+req.params.team);
+		    
+		    setInterval(function(){
+		    	console.log('checking scores');
+		    	var emitFlag = null;
+		    	if(lastGameState.hs < currentScores[gameIndex].hs){
+		    		emitFlag = 'homeScore';
+		    	} else if(lastGameState.vs < currentScores[gameIndex].vs){
+		    		emitFlag = 'awayScore';
+		    	}
+		    	if(emitFlag != null){
+		    		io.emit(emitFlag,{
+		    			'home': currentScores[gameIndex].hs,
+		    			'away': currentScores[gameIndex].vs
+		    		});
+		    	}
+		    }, 15000);
+
+		    socket.on('disconnect', function() {
+		    	console.log('user connected to '+req.params.team+' disconnected');
+		    });
+		});
+	}
+});
+
+/**
+* Makes a request to the url and does some janky things to the data...
+*/
+function updateScore() {
 	var score;
 	request({ 
 		method: 'get',
-		uri:'http://sports.espn.go.com/nfl/bottomline/scores'
+		uri:'http://www.nfl.com/liveupdate/scorestrip/ss.json'
 		},
 		function(error,response,body) {
-			var data = body.replace(/(^\?)/,'').split("&").map(function(n){return n = n.split("="),this[n[0]] = n[1],this}.bind({}))[0];
-			var dataVals = _.values(data);
-			var game = findString(dataVals);
-			var gameArr = game.split("%20");
-			currentScore = gameArr[gameArr.indexOf('Bay')+1];
+			currentScores = JSON.parse(body).gms;
+			console.log('Scores Updated');
 		}
 	);
 }
 
-setInterval(function(){
-	console.log(currentScore);
-	getScore();	
-},15000);
-
-io.on('connection', function(socket) {
-    console.log('a user connected');
-    var lastScore = currentScore;
-    socket.on('disconnect', function () {
-        console.log('user disconnected');
-    });
-    setInterval(function(){
-    	if(lastScore < currentScore || lastScore == undefined){
-    		var emitNum;
-    		if(lastScore == undefined){
-    			emitNum = 0;
-    		}else {
-    			emitNum = (currentScore-lastScore);
-    		}
-    		io.emit('score', emitNum);
-    		console.log('Emitted Chirps');
-    		lastScore = currentScore;
-    	}
-    }, 5000);
-});
-
 server.listen(3000, function() {
-    console.log('listening on *:3000');
+    request({ 
+		method: 'get',
+		uri:'http://www.nfl.com/liveupdate/scorestrip/ss.json'
+		}
+	).then(function(body){
+		var json = JSON.parse(body);
+		currentScores = json.gms;
+		console.log("Scores Set");
+		setInterval(function(){
+			updateScore();	
+		},30000);
+	});
+	console.log('listening on *:3000');
 });
 
